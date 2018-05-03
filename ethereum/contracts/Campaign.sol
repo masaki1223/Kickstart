35f7e4876ffe656b150cd4485488e6fd59c5d499
masaki <masakiminamide@gmail.com>
2018-05-03 15:47:11 +0200

    install modules for ethreum

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,84 @@
+pragma solidity^0.4.18;
+
+contract CampaignFactory {
+    address[] public deployedCampaign;
+
+    function createCampaign(uint _minimum) public payable {
+        address newCampaign = new Campaign(_minimum, msg.sender);
+        deployedCampaign.push(newCampaign);
+        }
+        function getDeployedCampaigns() public view returns(address[]){
+        return deployedCampaign;
+        }
+    }
+
+    contract Campaign{
+
+        struct Request {
+        string description;
+        uint value;
+        address recepient;
+        bool complete;
+        mapping(address => bool) approved;
+        uint approvalCount;
+        }
+
+        Request[] public requests;
+        address public manager;
+        uint public miniumContribution ;
+        uint public totalContribution;
+
+        mapping(address => bool) public approvers;
+        uint public approverCount = 0;
+
+        modifier onlyManager() {
+        require(msg.sender == manager);
+        _;
+        }
+
+        constructor(uint _minimum, address _manager) public {
+        miniumContribution = _minimum;
+        manager = _manager;
+        }
+        function contribute() public payable {
+        require(msg.value > miniumContribution && msg.sender != manager);
+        approvers[msg.sender] = true;
+        totalContribution += msg.value;
+        approverCount++;
+        }
+        function getTotalContribution() public view returns(uint){
+        return totalContribution;
+        }
+        function createRequest(string _description, uint _value, address _recipient) public onlyManager {
+        Request memory newRequest = Request({
+        description: _description,
+        value: _value,
+        recepient: _recipient,
+        complete: false,
+        approvalCount: 0
+        });
+        requests.push(newRequest);
+        }
+
+        function approveRequest(uint _index) public {
+        Request storage request = requests[_index];
+        require(
+        approvers[msg.sender] &&
+        !request.approved[msg.sender] &&
+        manager != msg.sender &&
+        !request.complete
+        );
+        request.approved[msg.sender] = true;
+        request.approvalCount++;
+        }
+
+        function finalizeRequest(uint _index) public onlyManager {
+            Request storage request = requests[_index];
+            require(!request.complete && request.approvalCount > (approverCount/2));
+            request.recepient.transfer(request.value);
+            request.complete = true;
+        }
+
+
+
+}
